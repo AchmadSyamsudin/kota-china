829a004d3aac34f9cce42ea4b01ba34741a07a92
Hakwashere <147596837+Hakwashere@users.noreply.github.com>
2024-05-02 20:52:21 +0700

    Update Tugas2.py

--- Tugas2.py
+++ Tugas2.py
@@ -11,3 +11,16 @@ def perjalanan(graf, awal, akhir):
                 stack.append((next_node, path + [next_node]))
 def print_rute(rute):
     for i, path in enumerate(rute, start=1):
+        print(f'Rute {i}: {" -> ".join(path)}')
+graf_kota_china = {
+    'Beijing': ['Tianjin', 'Shijiazhuang', 'Jinan'],
+    'Tianjin': ['Beijing', 'Shijiazhuang'],
+    'Shijiazhuang': ['Beijing', 'Tianjin', 'Zhengzhou', 'Xingtai'],
+    'Jinan': ['Beijing', 'Shijiazhuang', 'Zhengzhou', 'Qingdao'],
+    'Zhengzhou': ['Shijiazhuang', 'Jinan', 'Wuhan'],
+    'Nanjing': ['Jinan', 'Hefei', 'Shanghai'],
+    'Qingdao': ['Jinan', 'Taian'],
+    'Wuhan': ['Zhengzhou', 'Changsha', 'Nanchang'],
+    'Hefei': ['Nanjing', 'Wuhan'],
+    'Shanghai': ['Nanjing', 'Hangzhou']
+}
