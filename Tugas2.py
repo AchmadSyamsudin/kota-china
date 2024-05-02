7752355d4ef2576f08b7bc1c7b4701c7019fef38
AchmadSyamsudin <147597081+AchmadSyamsudin@users.noreply.github.com>
2024-05-02 20:41:49 +0700

    Update Tugas2.py

--- Tugas2.py
+++ Tugas2.py
@@ -9,7 +9,3 @@ def perjalanan(graf, awal, akhir):
                 yield path + [next_node]
             else:
                 stack.append((next_node, path + [next_node]))
-
-def print_rute(rute):
-    for i, path in enumerate(rute, start=1):
-        print(f'Rute {i}: {" -> ".join(path)}')
